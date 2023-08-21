a3c625d3d1c2f1a23cf65f8ee0e51da9a156e7cc
Georgia Bean-Butler <georgia.bean@icloud.com>
2023-08-21 16:58:47 +0200

    longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,43 @@
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def word_in_grid(word)
+    @answer.chars.sort.all? { |letter| @grid.include?(letter) }
+  end
+
+  def english_dictionary(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    data = resp.body
+    word = JSON.parse(data)
+    return word[:found]
+  end
+  
+  def score
+    @answer = params[:word]
+    @grid = params[:grid]
+    @valid_word = english_dictionary(@answer)
+    @letters_in_grid = word_in_grid(@answer)
+
+    if @valid_word && @letters_in_grid
+      @result = "Congratulation! #{@answer.upcase} is a valid English word."
+    elsif !@letters_in_grid
+      @result = "Sorry, but #{@answer.upcase} can’t be built out of the letters provided."
+    elsif !valid_word
+      @result = "Sorry, but #{answer.upcase} isn't a valid English word."
+    elsif !valid_word && letters_in_grid
+      @result = "Sorry, but #{answer.upcase} isn't a valid English word."
+    elsif valid_word && !letters_in_grid
+      @result = "Sorry, but #{@answer.upcase} can’t be built out of #{grid_letters}."
+    end
+  end
+
+
+end
